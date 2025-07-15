5b5d5925e3d2c3af4bcc19e76668612b6e1476a9
Vreem <andredevogel1993@gmail.com>
2025-07-15 22:26:57 +0200

    Start Rock Paper Scissors Project
    Write pseudocode for getComputerChoice function
    Use Math.random method
    
    Use if/else statements to log Rock Paper Scissors

---
+++ script.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+    return Math.random();
+        if (getComputerChoice = 0.25) {
+            console.log("Rock!");
+        }
+        if (getComputerChoice = 0.5) {
+            console.log("Paper!");
+        }
+        else {
+            console.log("Scissors!");
+        }
+}
