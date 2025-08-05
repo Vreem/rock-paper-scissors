7c2a71f2c85fab2c8e820bd974a2762e1bdf9d8d
Vreem <andredevogel1993@gmail.com>
2025-08-05 10:17:49 +0200

    Remove commit message comments in script.js

--- script.js
+++ script.js
@@ -9,16 +9,18 @@
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
         if (randomNumber == 0) {
+            console.log("Rock");
             return "Rock";
         }
         else if (randomNumber == 1) {
+            console.log("Paper");
             return "Paper";
         }
         else {
+            console.log("Scissors");
             return "Scissors";
         }
 }
-console.log(getComputerChoice());
 
 //  Step 3: Write the logic to get the human choice.
 //      -Create function getHumanChoice
@@ -28,20 +30,23 @@ console.log(getComputerChoice());
 
 function getHumanChoice() {
     const userInput = prompt('choose either Rock Paper or Scissors');
-        if (userInput === null) {
-            return "You chose to run away";
+        if (userInput == null) {
+            console.log("You chose to run away");
         }
-        else if (userInput == 'Rock') {
+        else if (userInput == "Rock") {
+            console.log("Rock");
             return "Rock";
         }
-        else if (userInput == 'Paper') {
+        else if (userInput == "Paper") {
+            console.log("Paper");
             return "Paper";
         }
         else {
+            console.log("Scissors");
             return "Scissors";
         }
 }
-console.log (getHumanChoice());
+
 
 //  Step 4: Declare the players score variables.
 //      -Create two variables named humanScore and computerScore in the global scope
@@ -49,6 +54,7 @@ console.log (getHumanChoice());
 
 let humanScore = 0;
 let computerScore = 0;
+console.log("Human Score " + humanScore, "Computer Score " + computerScore);
 
 //  Step 5: Write the logic to play a single round.
 //     -Create a function named playRound with parameters (humanChoice and Computerchoice).
@@ -60,32 +66,36 @@ let computerScore = 0;
 
 function playRound(humanChoice, computerChoice) {
     //WIN
-    if (humanChoice == "Rock" && computerChoice == "Scissors"
+    if (humanChoice == "Rock" && computerChoice === "Scissors"
         ||
-        humanChoice == "Paper" && computerChoice == "Rock"
+        humanChoice == "Paper" && computerChoice === "Rock"
         ||
-        humanChoice == "Scissors" && computerChoice == "Paper"
+        humanChoice == "Scissors" && computerChoice === "Paper"
     ) {
-        return "You win!";
+        console.log("You win!");
+        humanScore++;
     }
     //LOSE
-    else if (humanChoice == "Rock" && computerChoice == "Paper"
+    else if (humanChoice == "Rock" && computerChoice === "Paper"
         ||
-            humanChoice == "Paper" && computerChoice == "Scissors"
+            humanChoice == "Paper" && computerChoice === "Scissors"
         ||
-            humanChoice == "Scissors" && computerChoice == "Rock"
+            humanChoice == "Scissors" && computerChoice === "Rock"
     ) {
-        return "You lose!";
+        console.log("You lose!");
+        computerScore++;
     }
     //TIE
     else {
-        return "It's a draw!";
+        console.log("It's a draw!");
     }
 }
-
-console.log(playRound());
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
 playRound(humanSelection, computerSelection);
+
+console.log("Human Score " + humanScore, "Computer Score " + computerScore);
+
+
